062a7f53ae57b3e5aeb8dfa5b6f2c566e4e97e95
SebastianDion <sebastian.dion02@gmail.com>
2024-10-08 23:15:38 +0700

    update with Blog

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,6 +2,8 @@ import { icons } from "lucide-react";
 import Link from "next/link";
 
 import{FaGithub, FaLinkedinIn, FaYoutube, FaEnvelope,FaWhatsapp} from 'react-icons/fa'
+import { FaNewspaper } from "react-icons/fa6";
+
 const social =[
     //Linksssss
     {icon: <FaGithub />, path:"https://github.com/SebastianDion"},
@@ -9,6 +11,9 @@ const social =[
     {icon: <FaEnvelope />, path:"mailto:sebastian.dion02@gmail.com"},
     {icon: <FaWhatsapp />, path:"https://wa.me/qr/WWVM4UCKFDKPC1"},
     {icon: <FaYoutube />, path:"https://www.youtube.com/@xxassassinzz9544"},
+    {icon: <FaNewspaper />, path:"https://soft-chestnut-66c.notion.site/Seb-s-Blog-1194f82ca71e8011b36ce9efea061681"},
+    
+    
 
 ]
 const Social = ({ContainerStyles, iconStyles}) => {
